0b542a1811eaffc478d58ad1eb919d0c7938c1d1
Dan Hicks <hicks.daniel.j@gmail.com>
2017-12-13 11:05:22 -0800

    note the empty DOIs problem

--- get_pub.R
+++ get_pub.R
@@ -4,7 +4,8 @@ library(rcrossref)
 ## Load DOIs of interest
 dataf = read.csv('scopus.csv', stringsAsFactors = FALSE)
 dois = dataf$DOI
-# dois = dois[dois != '']
+# TODO: empty DOIs end up as NULL list entries, so we can't just unlist() the results
+dois = dois[dois != '']
 
 ## Retrieve metadata
 metadata = cr_cn(dois, format = 'citeproc-json', 
